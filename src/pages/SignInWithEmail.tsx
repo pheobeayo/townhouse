1a9eea7ff35ac9c9fd5e0d204e6b1e5fd81c5585
imrany <imranmat254@gmail.com>
2024-05-11 09:53:52 +0300

    Major UI update on sign in, sign in with email, sign up and sign up with email and sign in page

--- src/pages/SignInWithEmail.tsx
+++ src/pages/SignInWithEmail.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react"
-import backgroundPattern from "../assets/images/backgrounds/Background_pattern.png"
+import background from "../assets/images/backgrounds/Screen3.jpg";
+import backgroundPattern from "../assets/images/backgrounds/Background_pattern.svg";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { err_toast } from "../components/Feedback";
-import { Link } from "react-router-dom";
 import { User } from "../types/definitions"
 import { useNavigate } from "react-router-dom"
+import Logo from "../assets/images/logos/Logo2.svg"
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react"
 
 export default function SignInWithEmail() {
     let date=new Date()    
@@ -12,6 +14,19 @@ export default function SignInWithEmail() {
     let [eye_icon,setEye_icon]=useState(<FaEye className="h-5 w-5"/>);
     let [disable,setDisable]=useState(false); 
     let navigate=useNavigate()
+
+    let [screenBackground,setScreenBackground]=useState(``)
+    let [showLogo,setShowLogo]=useState(true)
+    window.onresize=()=>{
+        let screen_width=window.innerWidth;
+        if(screen_width>768){
+            setScreenBackground(`linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(${background}),no-repeat`)
+            setShowLogo(true)
+        }else{
+            setScreenBackground(`url(${backgroundPattern})`)
+            setShowLogo(false)
+        }
+    }
 
     function toggle_password(){
         let password=document.getElementById("password");
@@ -71,12 +86,28 @@ export default function SignInWithEmail() {
             errorMessage==="Failed to fetch"?err_toast(`No internet`):err_toast(error.message)
         }
     }
+
+    useEffect(()=>{
+        let screen_width=window.innerWidth;
+        if(screen_width>768){
+            setScreenBackground(`linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(${background}),no-repeat`)
+            setShowLogo(true)
+        }else{
+            setScreenBackground(`url(${backgroundPattern})`)
+            setShowLogo(false)
+        }
+    },[screenBackground])
     return (
-        <main style={{background:`url(${backgroundPattern})`}} className={`flex h-screen justify-center flex-col items-center`}>
-            <div className="sm:m-[40px] items-center sm:shadow-lg bg-white flex flex-col sm:w-[520px] max-sm:px-[6vw]">
-            <div className="sm:w-[360px] my-[40px]">
-                 <div className="sm:mb-[40px] gap-[8px] flex flex-col items-center max-sm:my-[20px]">
-                    <p className="text-[30px] font-semibold">Welcome back</p>
+        <main style={{background:`${screenBackground}`,backgroundPosition:'center',backgroundSize:'cover'}} className={`flex h-screen justify-center max-md:flex-col max-md:items-center `}>
+            {showLogo?(
+                <Link to="/" className="mr-auto h-full flex-grow flex items-center justify-center">
+                    <img src={Logo} className="w-[363px] h-[87px]" alt="logo"/>
+                </Link>
+            ):""}
+            <div className="md:ml-auto sm:m-[40px] items-center sm:rounded-sm sm:shadow-lg bg-white flex flex-col sm:w-[520px] max-sm:px-[4vw]">
+                <div className="sm:w-[360px] my-[40px]">
+                <div className="sm:mb-[40px] gap-[8px] flex flex-col items-center max-sm:my-[20px]">
+                    <p className="text-[30px] font-semibold">Welcome Back</p>
                     <p className="text-[var(--secondary-08)] text-[14px]">Get you connected with your community</p>
                 </div>
                 <form onSubmit={(e)=>handleLogin(e)} className="flex flex-col gap-[12px] text-sm">
